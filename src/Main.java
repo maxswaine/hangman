138442b54a1dd35f5d55d8baba2f5f677ea3d0f4
maxswaine <108192224+maxswaine@users.noreply.github.com>
2023-09-16 15:02:30 +0100

    Functional return array that gets the user input for the length of the word but needs to be amended for the length of the lives lost (#2)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        CaptureGuess  newGuess = new CaptureGuess();
+        String wordToGuess = Words.generateWord();
+
+        ArrayList<Character> charGuess = newGuess.captureGuess(wordToGuess);
+        System.out.println(charGuess);
     }
+
 }
